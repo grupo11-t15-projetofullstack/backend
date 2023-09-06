3a63f0cfd949c3debf26a9d5399a2c6cf3a3a06e
felipe <felipe@hcdesenvolvimento.com.br>
2023-09-06 16:26:58 -0300

    userid

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -30,6 +30,7 @@ export class AuthService {
 
       return {
         token: this.jwtService.sign(payload, jwtOptions),
+        id: user.id
       };
     }
     // Handle the case where user is not found
